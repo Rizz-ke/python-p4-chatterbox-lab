f0b4bb62c897649c1a6442d825d0d24cbd8f861d
Wafiq Mohamed <wafiqmohameddoo@gmail.com>
2024-07-17 14:55:01 +0300

    assignment completed

--- server/app.py
+++ server/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+from datetime import datetime
 
 from models import db, Message
+from sqlalchemy.orm import Session
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -14,13 +16,49 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    serialized_messages = [message.to_dict() for message in messages]
+    return jsonify(serialized_messages)
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages', methods=['POST'])
+def create_message():
+    body = request.json.get('body')
+    username = request.json.get('username')
+    if not body or not username:
+        return make_response(jsonify({'error': 'Missing body or username'}), 400)
+
+    new_message = Message(body=body, username=username)
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify(new_message.to_dict())
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    with Session(db.engine) as session:
+        message = session.get(Message, id)
+        if not message:
+            return make_response(jsonify({'error': 'Message not found'}), 404)
+
+        new_body = request.json.get('body')
+        if new_body:
+            message.body = new_body
+            message.updated_at = datetime.utcnow()
+            session.commit()
+        
+        return jsonify(message.to_dict())
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    with Session(db.engine) as session:
+        message = session.get(Message, id)
+        if not message:
+            return make_response(jsonify({'error': 'Message not found'}), 404)
+
+        session.delete(message)
+        session.commit()
+        return jsonify({'message': 'Message deleted'})
 
 if __name__ == '__main__':
     app.run(port=5555)
